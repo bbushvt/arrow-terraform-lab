734c6494d2a33183189cfdf071eef1a43ea38780
Bryan Bush <bbushvt@gmail.com>
2023-12-11 10:51:04 -0500

    changed sshkey in IBM from data to resource

--- ibm.tf
+++ ibm.tf
@@ -2,7 +2,7 @@ data "ibm_resource_group" "rg" {
   name = var.resource_group
 }
 
-data "ibm_is_ssh_key" "sshkey1" {
+resource "ibm_is_ssh_key" "sshkey1" {
   name = var.ssh_key_name
   public_key = var.ssh_public_key
   type = "rsa"
